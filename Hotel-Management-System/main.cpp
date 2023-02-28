9c21be4eaef43c3c89d93181c17626ff6116d453
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-02-28 12:22:38 +0000

    Add project files.

---
+++ Hotel-Management-System/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int quant;
+	int choice;
+
+	// Quantity
+	int Qrooms = 0;
+	int Qpasta = 0;
+	int Qburger = 0;
+	int Qnoodles = 0;
+	int Qshake = 0;
+	int Qchicken = 0;
+
+	// Food items sold
+	int Srooms = 0;
+	int Spasta = 0;
+	int Sburger = 0;
+	int Snoodles = 0;
+	int Sshake = 0;
+	int Schicken = 0;
+
+	// Total proce of items
+	int Total_tooms = 0;
+	int Total_pasta = 0;
+	int Total_burger = 0;
+	int Total_noodles = 0;
+	int Total_shake = 0;
+	int Total_chicken = 0;
+
+};
